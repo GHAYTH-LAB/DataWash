fe637430b70d993cf07f3205985ff18ed3fae7aa
GHAYTH ABIDLI <ghaythff0@gmail.com>
2026-07-20 22:25:53 +0100

    Clean and preprocess fashion dataset
    Removed unnecessary print statements and cleaned the dataset by renaming columns, handling missing values, and adjusting data types.

--- cleaning/fashion_cleaning.py
+++ cleaning/fashion_cleaning.py
@@ -1,25 +1,16 @@
 import pandas as pd
 import numpy as np
 df=pd.read_csv(r"C:\Users\abidli\Desktop\cleaning datasets\datasets\raw\FashionDataset.csv")
-#checking my dataset at first
-print(df.head())
-print(df.tail())
-print(df.info())
-print(df.shape)
 df=df.drop(columns="Unnamed: 0")
-print(df.duplicated().sum())
 df=df.replace("Nan",np.nan)
-print(df.isna().sum())
 df=df.drop_duplicates()
 df["SellPrice"]=df["SellPrice"].astype(float)
-print(df.dtypes)
 df=df.fillna({
     "BrandName":df["BrandName"].mode()[0]
     ,"Deatils":df["Deatils"].mode()[0]
     ,"SellPrice":df["SellPrice"].median()
 })
 df=df.dropna(subset=["MRP","Discount","Sizes"])
-print(df.columns)
 df.columns=(df.columns
             .str.lower()
             .str.strip()
@@ -27,7 +18,6 @@ df.columns=(df.columns
 df=df.rename(columns={
     "deatils":"details"
 })
-print(df.columns)
 Q1=df['sellprice'].quantile(0.25)
 Q3=df["sellprice"].quantile(0.75)
 IQR=Q3-Q1
@@ -47,9 +37,7 @@ df["mrp"]=df["mrp"].str.split("\n",n=1).str[1]
 df["mrp"]=df["mrp"].astype(int)
 df=df.rename(columns={"discount":"discount%"})
 df["discount%"]=df["discount%"].str.split("%",n=1).str[0]
-print(df["discount%"])
 df=df.rename(columns={"category":"category women"})
 df["category women"]=df["category women"].str.split("-",n=1).str[0]
-print(df["category women"])
 df["sellprice"]=df["sellprice"].astype(int)
 df.to_csv("fasion dataset cleaned.csv",index=False)
