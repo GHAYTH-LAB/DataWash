9ce092b85aa18e63e8dcd6b173e474ef71fb3932
GHAYTH ABIDLI <ghaythff0@gmail.com>
2026-07-20 22:27:27 +0100

    Optimize data processing in netflix_cleaning.py
    Refined data cleaning steps and enhanced feature extraction.

--- cleaning/netflix_cleaning.py
+++ cleaning/netflix_cleaning.py
@@ -1,25 +1,15 @@
 import pandas as pd
 df=pd.read_csv(r"C:\Users\abidli\Desktop\cleaning datasets\datasets\raw\netflix1.csv")
-print(df.shape)
-print("---------")
-#check for duplicates and na values (0 for both cases)
-"""print(df.isna().sum())
-print(df.duplicated.sum())"""
-#standarizing columns names
-print(df.columns)
 df.columns=df.columns.str.strip()
 df.columns=df.columns.str.lower()
 df.columns=df.columns.str.replace("_"," ")
-#checking for types
 df["date added"]=pd.to_datetime(df["date added"])
-print(df.dtypes)
 #checking for outliers
 Q1=df["release year"].quantile(0.25)
 Q3=df["release year"].quantile(0.75)
 IQR=Q3-Q1
 Lower_band=Q1-1.5*IQR
 Higher_band=Q3+1.5*IQR
-print(df.shape)
 df=df[(df["release year"]>Lower_band) & (df["release year"]<Higher_band)]
 print(df.shape)
 #some feature engineering (extracting day and month and year from release year column)
@@ -27,9 +17,6 @@ df["day"]=df["date added"].dt.day
 df["month"]=df["date added"].dt.month
 df["year"]=df["date added"].dt.year
 df=df.drop(columns="date added")
-print(df.columns)
-print((df["director"]=="Not Given").sum())
-print(df.shape)
 df["director"]=(df["director"]
                 .replace("Not Given","unknown")
                 .str.lower()
